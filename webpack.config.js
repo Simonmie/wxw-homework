a897a00fdc8fb705a9f3eec7b0cc3aa6b8abe78e
Yran-nloy7 <1426907007@qq.com>
2026-03-17 00:19:23 +0800

    fix: 添加webpack配置和gitignore

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,32 @@
-// src/webpack.config.js
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  // 入口
   entry: './index.js',
 
-  // 输出
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true // 每次打包前自动清空 dist 文件夹
+    clean: true,
+    assetModuleFilename: '[name][ext]'
   },
 
-  // 模式
   mode: 'development',
 
-  // 处理 CSS、图片、字体
   module: {
     rules: [
-      // CSS 
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'] // 顺序不能颠倒：先解析 CSS，再注入页面
+        use: ['style-loader', 'css-loader']
       },
-      // 图片
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
         generator: {
-          filename: 'images/[name][ext]' 
+          filename: 'images/[name][ext]'
         }
       },
-      // 字体
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
@@ -43,19 +37,23 @@ module.exports = {
     ]
   },
 
-  // 插件
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.html', // 现有为模板
-      filename: 'index.html'    // 打包后生成HTML
-    })
+      template: './index.html',
+      filename: 'index.html'
+    }),          // ← 逗号在这里
+    new CopyPlugin({
+      patterns: [
+        { from: 'images', to: 'images' },
+        { from: 'fonts', to: 'fonts' },
+      ],
+    }),
   ],
 
-  // 开发配置
   devServer: {
-    port: 3000,    // 访问地址：http://localhost:3000
-    open: true,    // 启动后自动打开
-    hot: true,     // 代码更新自动刷新页面
+    port: 3000,
+    open: true,
+    hot: true,
     static: {
       directory: path.join(__dirname, 'dist')
     }
