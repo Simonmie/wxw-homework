e9edda7f75217e96b685a5ea04e3e31f9b443edb
Yran-nloy7 <1426907007@qq.com>
2026-03-25 23:28:26 +0800

    feat:配置eslint + prettier + husk

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-prettier'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    'prettier/prettier': [
+      'warn',
+      {
+        singleQuote: true,
+        semi: false,
+        printWidth: 80,
+        trailingComma: 'none',
+        endOfLine: 'auto'
+      }
+    ],
+    'vue/multi-word-component-names': ['off'],
+    'vue/no-setup-props-destructure': ['off'],
+    'no-undef': 'error'
+  }
+}
